fcc41754ab0fa9194ab13b6eb7d2f0c6ddf0d036
truong2305 <truongnv18dt1bkdn@gmail.com>
2022-06-05 15:17:59 +0700

    add

---
+++ js/search.js
@@ -0,0 +1,60 @@
+// console.clear();
+
+
+let contentTitle;
+
+function dynamicClothingSection(ob) {
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
+
+  let boxLink = document.createElement("a");
+  // boxLink.href = '#'
+  boxLink.href = "/contentDetails.html?" + ob.id;
+  // console.log('link=>' + boxLink);
+
+  let imgTag = document.createElement("img");
+  // imgTag.id = 'image1'
+  // imgTag.id = ob.photos
+  imgTag.src = ob.preview;
+
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.name);
+  h3.appendChild(h3Text);
+
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.brand);
+  h4.appendChild(h4Text);
+
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode((ob.price).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }));
+  h2.appendChild(h2Text);
+
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
+
+  return boxDiv;
+}
+
+//  TO SHOW THE RENDERED CODE IN CONSOLE
+// console.log(dynamicClothingSection());
+
+// console.log(boxDiv)
+
+let containerClothing = document.getElementById("containerClothing");
+
+// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
+
+// BACKEND CALLING
+console.log(data);
+containerClothing.appendChild(
+    dynamicClothingSection(data))
